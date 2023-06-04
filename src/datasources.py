96493c36e4ee2d526b0ccc5581dbee027cc5b181
Elliot Marx <elliot@chalk.ai>
2023-06-04 11:03:50 -0700

    datasources

--- src/datasources.py
+++ src/datasources.py
@@ -1,5 +1,13 @@
 from chalk.sql import SnowflakeSource, PostgreSQLSource
 
 
+# These data sources can be used as singletons for querying
+# data. The credentials for these data sources have been
+# added in the Chalk dashboard under the "Data Sources" tab.
+#
+# They can alternatively be configured with environment
+# variables, or if you have many data sources of the same type,
+# you can assign configuration variables to a data source
+# name, and provide that name in the constructors below.
 snowflake = SnowflakeSource()
 postgres = PostgreSQLSource()
